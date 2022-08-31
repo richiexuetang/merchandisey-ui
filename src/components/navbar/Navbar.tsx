d4ff1838120c205cd4de4906e19ef0573ee6a65f
richiexuetang <richard772794659@gmail.com>
2022-08-30 22:10:49 -0700

    redo nav input

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import * as Styled from './navbar.styles';
-import SearchIcon from '@mui/icons-material/Search';
 import { ReactComponent as MerchYLogo } from '../../assets/svgs/logo.svg';
 import { Outlet } from 'react-router-dom';
 import { NavMenu } from './nav-menu';
 import Menubar from '../menubar/Menubar';
+import NavInput from './NavInput';
 
 const Navbar = () => (
   <React.Fragment>
@@ -20,17 +20,7 @@ const Navbar = () => (
         </Styled.NavLogoContainer>
         {/* End of Logo */}
 
-        {/* Searbar */}
-        <Styled.SearchbarContainer>
-          <Styled.Wrapper flexDirection='row' alignItems='normal'>
-            <Styled.Search>
-              <SearchIcon />
-            </Styled.Search>
-            <Styled.Searchbox placeholder='Search for brand, color, etc.' />
-          </Styled.Wrapper>
-        </Styled.SearchbarContainer>
-        {/* End of Searchbar */}
-
+        <NavInput />
         <NavMenu />
       </Styled.Container>
     </Styled.SiteHeader>
